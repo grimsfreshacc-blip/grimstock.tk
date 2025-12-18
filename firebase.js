22d9ebed6b4abafdddbcb30b017af27b785ef98e
grimsfreshacc-blip <grimsfreshacc@gmail.com>
2025-12-17 21:42:30 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,17 @@
 <script type="module">
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyB5ol1sETCQyVlVju6StIKgGDsYWuhjfyM",
-    authDomain: "grimstock-74304.firebaseapp.com",
-    projectId: "grimstock-74304",
-    storageBucket: "grimstock-74304.firebasestorage.app",
-    messagingSenderId: "515037820272",
-    appId: "1:515037820272:web:5ce3c182a069a54a72cbb5"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyB5ol1sETCQyVlVju6StIKgGDsYWuhjfyM",
+  authDomain: "grimstock-74304.firebaseapp.com",
+  projectId: "grimstock-74304",
+  storageBucket: "grimstock-74304.firebasestorage.app",
+  messagingSenderId: "515037820272",
+  appId: "1:515037820272:web:5ce3c182a069a54a72cbb5"
+};
 
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-
-  window.firebaseAuth = auth;
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+window.firebaseAuth = auth;
 </script>
